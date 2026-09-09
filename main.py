103ffa245814bc8ee99474b43051f8721759a129
nsbuman <nicholasbuman94@gmail.com>
2026-09-09 02:38:14 -0400

    fix donwload step

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def go(config: DictConfig):
                 env_manager="conda",
                 parameters={
                     "sample": config["etl"]["sample"],
-                    "artifact_name": config["etl"]["sample"],
+                    "artifact_name": "sample.csv",
                     "artifact_type": "raw_data",
                     "artifact_description": "Raw file as downloaded"
                 },
