a50b073437597fcff7cfb7999ab731d9a4ee1b70
nsbuman <nicholasbuman94@gmail.com>
2026-09-09 02:13:21 -0400

    Ensure project is set to the public W&B

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def go(config: DictConfig):
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
+    os.environ["WANDB_PROJECT"] = "nyc_airbnb_public"
 
     # Steps to execute
     steps_par = config['main']['steps']
